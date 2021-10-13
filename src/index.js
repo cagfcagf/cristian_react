72f6e1a06cb928ce79be7533231b087d8a10a3eb
cagfcagf <cagfeijoo@gmail.com>
2021-10-13 13:35:30 -0300

    primera subida

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Elemento = () => <p> Hola Mundo </p>
+
+ReactDOM.render( <Elemento/>,  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
