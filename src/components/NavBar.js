38a9c20e61874fc2081b1ece7ff6d6d5e78ef16c
Cristian <cagfeijoo@gmail.com>
2021-10-16 22:17:58 -0300

    menu de ecommerce

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <header>
+      <h1>GuiltyBox</h1>
+      <nav>
+        <a href=''>¿Quienes somos?</a>
+        <a href=''> Politicas de devoluciones</a>
+        <a href=''> Comprar</a>
+      </nav>
+
+    </header>
+  )
+}
+
+export default Navbar
